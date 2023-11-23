802dc4d531e52503945645d9b7e4737a9302b8f4
AeroAstroid <aeroastroid@gmail.com>
2023-11-22 22:39:07 -0300

    First voting screen implementation

---
+++ pentagon/voting.js
@@ -0,0 +1,23 @@
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+const urlParams = new URLSearchParams(window.location.search);
+let screen = urlParams.get("screen");
+
+console.log(screen);
+if (screen != null) {
+    let selected_entries = urlParams.get("screen").split("/");
+    let entry_list;
+
+    fetch('entries.txt')
+    .then(r => r.text())
+    .then(text => {
+        entry_list = text.split("\r\n");
+
+        console.log(entry_list);
+        for (e = 0; e < selected_entries.length; e++) {
+            let entry_n = parseInt(selected_entries[e]) - 1;
+            let div_code = "<tr><th class='letter'>" + alphabet[e] + "</th><th>" + entry_list[entry_n] + "</th></tr>"
+            document.getElementById("screen").innerHTML += div_code;
+        }
+    })
+}
