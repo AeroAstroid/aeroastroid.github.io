1b7e239f905f92a40a89093bd44f1fc34e10ade0
AeroAstroid <aeroastroid@gmail.com>
2023-11-23 02:03:20 -0300

    voter.js votelink implementation + style

--- pentagon/voting.js
+++ pentagon/voting.js
@@ -11,11 +11,22 @@ if (screen != null) {
     .then(r => r.text())
     .then(text => {
         entry_list = text.split("\n");
+        votelink = "https://voter.figgyc.uk/#votelink3=%09%09";
 
         for (e = 0; e < selected_entries.length; e++) {
             let entry_n = parseInt(selected_entries[e]) - 1;
             let div_code = "<tr><th class='letter'>" + alphabet[e] + "</th><th class='response'>" + entry_list[entry_n] + "</th></tr>"
             document.getElementById("screen").innerHTML += div_code;
+
+            votelink += encodeURI(alphabet[e]);
+            votelink += "%09";
+            votelink += encodeURI(entry_list[entry_n]);
+
+            if (e < selected_entries.length - 1) {
+                votelink += "%0A";
+            }
         }
+
+        document.getElementById("votelink").setAttribute("href", votelink);
     })
 }
